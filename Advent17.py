2120a48266eb64dcb7585537e34548aa099e7630
Benjamin ZERATH <benjamin.zerath-ext@transport.alstom.com>
2015-12-17 09:08:06 +0100

    17e jour, part 2, first try

--- Advent17.py
+++ Advent17.py
@@ -9,13 +9,21 @@ from itertools import permutations, combinations
 if __name__ == "__main__":
     #containers = [20, 15, 10, 5, 5]
     containers = [43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34, 17, 17, 44, 36, 31, 46, 9, 27, 38]
+    mini_size = len(containers)
     nombre = 0
     for i in range(len(containers)):
         for combi in combinations(containers, i):
             if sum(combi) == 150:
-                nombre += 1
-                #print combi
+                mini_size = min(len(combi), mini_size)
+
+    for combi in combinations(containers, mini_size):
+        if sum(combi) == 150:
+            nombre += 1
+
+
 
     print nombre
+
+
 
 
