eecdb6244e7dd0bafc0dcf1a4894b95c7ace2023
Benjamin ZERATH <benjamin.zerath-ext@transport.alstom.com>
2015-12-17 09:20:20 +0100

    17e jour, part 2, better version.

--- Advent17.py
+++ Advent17.py
@@ -1,28 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals  # Everything is UTF-8
 from itertools import permutations, combinations
+import timeit
 
 
+def mini_size_of_solution(containers, value):
+    for solution_size in xrange(len(containers)):
+        for combi in combinations(containers, solution_size):
+            if sum(combi) == value:
+                return solution_size
+
+def chrono():
+    """
+    better one.
+    :return:
+    """
+    containers = [43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34, 17, 17, 44, 36, 31, 46, 9, 27, 38]
+    value = 150
+    nombre = 0
+    mini_size = mini_size_of_solution(containers, value)
+    for combi in combinations(containers, mini_size):
+        if sum(combi) == value:
+            nombre += 1
+    return nombre
+
+def chrono2():
+    containers = [43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34, 17, 17, 44, 36, 31, 46, 9, 27, 38]
+    value = 150
+    mini_size = len(containers)
+    nombre = 0
+    for i in range(len(containers)):
+        for combi in combinations(containers, i):
+            if sum(combi) == value:
+                mini_size = min(len(combi), mini_size)
+    for combi in combinations(containers, mini_size):
+        if sum(combi) == value:
+            nombre += 1
+    return nombre
 
 
 
 if __name__ == "__main__":
     #containers = [20, 15, 10, 5, 5]
-    containers = [43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34, 17, 17, 44, 36, 31, 46, 9, 27, 38]
-    mini_size = len(containers)
-    nombre = 0
-    for i in range(len(containers)):
-        for combi in combinations(containers, i):
-            if sum(combi) == 150:
-                mini_size = min(len(combi), mini_size)
-
-    for combi in combinations(containers, mini_size):
-        if sum(combi) == 150:
-            nombre += 1
 
 
-
-    print nombre
+    print "old =", timeit.timeit(chrono2, number=30)
+    print "new =", timeit.timeit(chrono, number=30)
 
 
 
